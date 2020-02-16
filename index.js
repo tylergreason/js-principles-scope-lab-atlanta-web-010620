ec26a64d6d03c5327a34ceafcd3274a63753993e
tylergreason <tyler.greason@protonmail.com>
2020-02-16 10:29:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'idk man';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'someone else';
+}
